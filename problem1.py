d11e4fd54bf61e678d7ebac3db43e4f68730f4de
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2025-12-03 13:26:25 +0530

    Adding Two Numbers

--- problem1.py
+++ problem1.py
@@ -1,11 +1,20 @@
 # Adding Two Numbers
 
 # INPUT
+# Method - 1
+print("Method - 1")
 a = 10
 b = 5
 c = a + b
 print(c)
 
+# Method - 2
+print("Method - 2")
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+sum = num1 + num2
+print("Addition is ",sum)
 
 # OUTPUT
 # 15
+# Based on User input
