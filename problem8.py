a0561d91f48518ec429d760771f198308ab4bd27
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2025-12-03 14:23:46 +0530

    ODD or EVEN

---
+++ problem8.py
@@ -0,0 +1,11 @@
+# ODD or EVEN
+
+# INPUT
+num = int(input("Enter a Number : "))
+if num % 2 == 0:
+    print("Number is Even")
+else : 
+    print("Number is Odd")
+
+# OUTPUT
+# Based on user input 
