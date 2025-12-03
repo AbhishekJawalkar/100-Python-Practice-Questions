e847f3eeae25a7ff3e661b57da2a5020fb099ff0
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2025-12-03 13:33:10 +0530

    Finding Area of a Traingle

---
+++ problem4.py
@@ -0,0 +1,17 @@
+# Finding Area of a Traingle
+
+# INPUT
+print("Finding Area of a Traingle")
+print()
+base = int(input("Enter the base of the traingle : "))
+height = int(input("Enter the height of the traingle : "))
+area = (1/2) * base * height
+print("The area of the triangle is :", area)
+
+
+# OUTPUT
+# Based on User input
+
+
+
+
